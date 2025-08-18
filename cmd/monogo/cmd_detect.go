6aa291b5c6bffb1a82de00894c3e07e2a6fbee31
Bruno Luiz Silva <contact@brunoluiz.net>
2025-08-18 22:59:30 +0100

    feat: initial walker implementation

--- cmd/monogo/cmd_detect.go
+++ cmd/monogo/cmd_detect.go
@@ -1,15 +1,68 @@
 package main
 
 import (
+	"fmt"
+	"path/filepath"
+
+	"github.com/concordalabs/monogo/internal/walker"
 	"github.com/concordalabs/monogo/xgit"
+	"github.com/samber/lo"
+	"golang.org/x/tools/go/packages"
 )
 
 type DetectCmd struct {
-	Path string `help:"Path to detect changes" default:"."`
+	Path        string   `help:"Path to detect changes" default:"."`
+	Entrypoints []string `help:"Entrypoints to analyze for changes" default:"./cmd/xpdig"`
 }
 
 func (r *DetectCmd) Run(c *Context) error {
-	x, err := xgit.Diff("main", xgit.WithPath(r.Path))
-	c.Logger.Info("Detected changes", "changes", x)
+	changesArr, err := xgit.Diff("main", xgit.WithPath(r.Path))
+	if err != nil {
+		return fmt.Errorf("failed to load diff: %v", err)
+	}
+	c.Logger.Info("Changed files", "files", changesArr)
+	changedPkgs, err := packages.Load(&packages.Config{
+		Mode: packages.NeedName,
+		Dir:  r.Path,
+	}, lo.Map(changesArr, func(change string, _ int) string {
+		return filepath.Join(r.Path, filepath.Dir(change))
+	})...)
+	if err != nil {
+		return fmt.Errorf("failed to load packages: %v", err)
+	}
+
+	for _, entry := range r.Entrypoints {
+		w, err := walker.New(r.Path)
+		if err != nil {
+			return err
+		}
+
+		matcher := changedPackageMatcher{changedPkgs: changedPkgs}
+		if err = w.Walk(c.Context, entry, matcher.Match); err != nil {
+			return err
+		}
+
+		if matcher.found {
+			c.Logger.Info("Changed entrypoint", "entrypoint", entry)
+		}
+	}
+
 	return err
+}
+
+type changedPackageMatcher struct {
+	changedPkgs []*packages.Package
+	found       bool
+}
+
+func (m *changedPackageMatcher) Match(p *packages.Package) (bool, error) {
+	if m.found {
+		return true, nil
+	}
+
+	_, ok := lo.Find(m.changedPkgs, func(changedPkg *packages.Package) bool {
+		return changedPkg.ID == p.ID
+	})
+	m.found = ok
+	return ok, nil
 }
---
+++ internal/walker/walker.go
@@ -0,0 +1,98 @@
+package walker
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"golang.org/x/mod/modfile"
+	"golang.org/x/tools/go/packages"
+)
+
+type Walker struct {
+	cache    map[string]*packages.Package
+	basePath string
+	module   string
+}
+
+func New(basePath string) (*Walker, error) {
+	module, err := getModuleName(basePath)
+	if err != nil {
+		return nil, fmt.Errorf("base path might not be a module: %w", err)
+	}
+
+	return &Walker{
+		cache:    make(map[string]*packages.Package),
+		basePath: basePath,
+		module:   module,
+	}, nil
+}
+
+type matcher func(p *packages.Package) (stop bool, err error)
+
+func (w *Walker) Walk(ctx context.Context, entry string, matchers ...matcher) error {
+	return w.walk(ctx, entry, matchers...)
+}
+
+func (w *Walker) walk(ctx context.Context, entry string, matchers ...matcher) error {
+	select {
+	case <-ctx.Done():
+		return nil
+	default:
+		// Load all packages in the codebase
+		pkgs, err := packages.Load(&packages.Config{
+			// Mode: packages.NeedImports | packages.NeedFiles | packages.NeedDeps | packages.NeedEmbedFiles | packages.NeedEmbedPatterns | packages.NeedName | packages.NeedExportFile,
+			// Mode: packages.NeedImports | packages.NeedEmbedFiles | packages.NeedEmbedPatterns | packages.NeedName,
+			Mode: packages.NeedImports | packages.NeedName,
+			Dir:  w.basePath,
+		}, entry)
+		if err != nil {
+			return fmt.Errorf("failed to load packages: %v", err)
+		}
+
+		// Iterate through all packages to find dependencies
+		for _, pkg := range pkgs {
+			for _, imported := range pkg.Imports {
+				if _, found := w.cache[imported.PkgPath]; found {
+					continue
+				}
+
+				if !strings.HasPrefix(imported.PkgPath, w.module) {
+					continue
+				}
+
+				// fmt.Printf("%s\n", imported)
+				w.cache[imported.PkgPath] = imported
+
+				for _, m := range matchers {
+					stop, err := m(imported)
+					if stop {
+						return nil
+					}
+					if err != nil {
+						return err
+					}
+				}
+
+				if err := w.walk(ctx, imported.PkgPath, matchers...); err != nil {
+					return fmt.Errorf("error finding dependent files for package %s: %s", pkg.PkgPath, err)
+				}
+			}
+		}
+
+		return nil
+	}
+}
+
+// getModuleName extracts the package path of a Go file.
+func getModuleName(filePath string) (string, error) {
+	data, err := os.ReadFile(filepath.Join(filePath, "go.mod"))
+	if err != nil {
+		fmt.Printf("Failed to read file: %v\n", err)
+		return "", nil
+	}
+
+	return modfile.ModulePath(data), nil
+}
